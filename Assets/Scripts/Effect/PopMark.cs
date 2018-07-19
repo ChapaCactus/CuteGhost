c0b0d180c246c2d1cdcf14a6c69543af7950a0f7
Chapa.C(Takuya) <chapatown@gmail.com>
2018-07-20 00:00:47 +0900

    ！マーク追加

---
+++ Assets/Scripts/Effect/PopMark.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace CCG
+{
+    /// <summary>
+    /// 「！」マーク等
+    /// </summary>
+    public class PopMark : MonoBehaviour
+    {
+        #region properties
+        private SpriteRenderer spriteRenderer { get; set; }
+        #endregion
+
+        #region private methods
+        private void Awake()
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+        #endregion
+
+        #region public methods
+        public void SetActive(bool isActive)
+        {
+            spriteRenderer.enabled = isActive;
+        }
+        #endregion
+    }
+}
