9e9c9813cc98f8067c86a70d5772ac0842ecb793
Chapa.C(Takuya) <chapatown@gmail.com>
2018-07-28 00:28:37 +0900

    キャラクターアニメーション設定ScriptableObject作成

---
+++ Assets/Scripts/ScriptableObjects/SpriteSetting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace CCG
+{
+    [CreateAssetMenu(menuName = "Assets/Create/SpriteSetting")]
+    public class SpriteSetting : ScriptableObject
+    {
+        #region variables
+        [SerializeField]
+        private List<Sprite> idleSprites = new List<Sprite>();
+        #endregion
+
+        #region properties
+        public List<Sprite> IdleSprites { get { return idleSprites; } }
+        #endregion
+    }
+}
