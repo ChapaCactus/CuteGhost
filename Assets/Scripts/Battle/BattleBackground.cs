9aef69a022f6a8253a075096e46fac7b09909b54
Chapa.C(Takuya) <chapatown@gmail.com>
2018-07-31 22:05:30 +0900

    2DFX追加、戦闘背景追加

---
+++ Assets/Scripts/Battle/BattleBackground.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+using DG.Tweening;
+
+namespace CCG
+{
+    public class BattleBackground : MonoBehaviour
+    {
+        #region variables
+        [SerializeField]
+        private _2dxFX_StoneFX fx = null;
+        #endregion
+
+        #region unity callbacks
+        private void Awake()
+        {
+            DOTween.To(() => fx.Deep, change => fx.Deep = change, 0.4f, 3.0f)
+                   .SetEase(Ease.Linear)
+                   .SetLoops(-1, LoopType.Yoyo);
+        }
+        #endregion
+    }
+}
