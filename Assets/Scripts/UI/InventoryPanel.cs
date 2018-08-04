4031abb8aab8e4ad2d09e066819b512356891f40
Chapa.C(Takuya) <chapatown@gmail.com>
2018-08-04 13:30:11 +0900

    CanvasGroupの表示設定、リスト用セルUIパーツ追加

--- Assets/Scripts/UI/InventoryPanel.cs
+++ Assets/Scripts/UI/InventoryPanel.cs
@@ -5,13 +5,36 @@ using UnityEngine.Assertions;
 
 namespace CCG
 {
+    [RequireComponent(typeof(CanvasGroup))]
     public class InventoryPanel : MonoBehaviour
     {
+        #region variables
+        [SerializeField]
+        private CanvasGroup canvasGroup = null;
+        #endregion
+
+        #region properties
+        private List<InventoryCell> Cells { get; set; }
+        #endregion
+
+        #region unity callbacks
+        #endregion
+
         #region public methods
         public void SetActive(bool isActive)
         {
-            gameObject.SetActive(isActive);
+            Assert.IsNotNull(canvasGroup);
+
+            if (!gameObject.activeSelf)
+                gameObject.SetActive(true);
+
+            canvasGroup.alpha = isActive ? 1 : 0;
+            canvasGroup.interactable = isActive;
+            canvasGroup.blocksRaycasts = isActive;
         }
+        #endregion
+
+        #region private methods
         #endregion
     }
 }
