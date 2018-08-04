bb1515879c3f5b148477996d0d727fa10f450468
Chapa.C(Takuya) <chapatown@gmail.com>
2018-08-04 13:40:59 +0900

    インベントリの表示

--- Assets/Scripts/UI/InventoryPanel.cs
+++ Assets/Scripts/UI/InventoryPanel.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
 
+using UnityEngine.UI;
+using System.Linq;
+
 namespace CCG
 {
     [RequireComponent(typeof(CanvasGroup))]
@@ -11,16 +14,37 @@ namespace CCG
         #region variables
         [SerializeField]
         private CanvasGroup canvasGroup = null;
+
+        [SerializeField]
+        private GridLayoutGroup layout = null;
         #endregion
 
         #region properties
-        private List<InventoryCell> Cells { get; set; }
+        private List<InventoryCell> Cells { get; set; } = new List<InventoryCell>();
         #endregion
 
         #region unity callbacks
+        private void Start()
+        {
+            Open(10);
+        }
         #endregion
 
         #region public methods
+        public void Open(int cellCount)
+        {
+            Cells.Clear();
+
+            var cellPrefab = Resources.Load<InventoryCell>("Prefabs/UI/InventoryCell");
+            foreach(var loop in Enumerable.Range(0, cellCount))
+            {
+                var cell = Instantiate(cellPrefab, layout.transform);
+                Cells.Add(cell);
+            }
+
+            SetActive(true);
+        }
+
         public void SetActive(bool isActive)
         {
             Assert.IsNotNull(canvasGroup);
