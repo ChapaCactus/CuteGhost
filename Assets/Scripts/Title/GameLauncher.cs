e0493a6429369a5cd17c9f3fac0f361968ce14e9
Chapa.C(Takuya) <chapatown@gmail.com>
2018-08-08 23:24:37 +0900

    ゲーム開始クラス作成

---
+++ Assets/Scripts/Title/GameLauncher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace CCG
+{
+    /// <summary>
+    /// ゲーム開始クラス
+    /// </summary>
+    public class GameLauncher : MonoBehaviour
+    {
+        #region unity callbacks
+        private void Awake()
+        {
+            Global.Init();
+
+            Debug.Log("Game Launched.");
+        }
+        #endregion
+    }
+}
