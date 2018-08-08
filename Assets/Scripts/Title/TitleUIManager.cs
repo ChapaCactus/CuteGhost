7171dce45072544dd3244f8d0f86d481e067df3c
Chapa.C(Takuya) <chapatown@gmail.com>
2018-08-08 23:19:44 +0900

    UIManager追加

---
+++ Assets/Scripts/Title/TitleUIManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace CCG
+{
+    public class TitleUIManager : MonoBehaviour
+    {
+        #region variables
+        [SerializeField]
+        private Button newGameButton = null;
+
+        [SerializeField]
+        private Button continueGameButton = null;
+
+        [SerializeField]
+        private Button fishingGameButton = null;
+        #endregion
+
+        #region unity callbacks
+        private void Awake()
+        {
+            InitUI();
+        }
+        #endregion
+
+        #region private methods
+        private void InitUI()
+        {
+            newGameButton.onClick.AddListener(OnClickNewGameButton);
+            continueGameButton.onClick.AddListener(OnClickContinueButton);
+            fishingGameButton.onClick.AddListener(OnClickFishingGameButton);
+        }
+
+        private void OnClickNewGameButton()
+        {
+            SceneManager.LoadScene("Main");
+        }
+
+        private void OnClickContinueButton()
+        {
+            SceneManager.LoadScene("Main");
+        }
+
+        private void OnClickFishingGameButton()
+        {
+            SceneManager.LoadScene("Main");
+        }
+        #endregion
+    }
+}
